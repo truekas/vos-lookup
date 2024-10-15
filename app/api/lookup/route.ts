b6be847594eddc62b208ecb9f392624c0a88bac3
KAS <karimalahmed3@gmail.com>
2024-10-14 21:27:30 -0400

    unfreeze lockfile attempt 3

--- app/api/lookup/route.ts
+++ app/api/lookup/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from 'next/server';
 import { meowMeow394 } from '@/lib/lookup';
 
+export const runtime = 'edge';
+
 export async function POST(request: Request) {
   const { code } = await request.json();
   const lookupdata = await meowMeow394(code)
